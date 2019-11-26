9afd5abf69126e33a4dbf61ba5b2867108d3dbc0
pooja <poojasrinivas24@gmail.com>
2019-11-26 12:08:46 +0530

    added new domain

--- core/symbolicEnvironment.py
+++ core/symbolicEnvironment.py
@@ -467,3 +467,88 @@ class TicTacTeo(SymbolicEnvironment):
             return 0, True
         return 0, False
 
+LPUSH = Predicate("lpush", 0)       #left push the pole, action with force = -10
+RPUSH = Predicate("rpush", 0)       #right push the pole, action with force = 10
+CARPOS = Predicate("carpos",1)      #car position while moving with a velocity
+CARVEL = Predicate("carvel", 1)     #car velocity is varying
+POLEANG = Predicate("poleang", 1)   #pole angle from the upright centre
+POLEVEL = Predicate("polevel", 1)   #pole velocity at tip of pole
+
+class cartPole(SymbolicEnvironment):
+    def __init__(self):
+        actions = [LPUSH, RPUSH]
+        self.gravity = 9.8
+        self.masscart = 1.0
+        self.masspole = 0.1
+        self.total_mass = (self.masscart + self. masspole)
+        self.polemass_length = 0.5
+        self.force_mag = 10
+        self.theta_threshold = 15*math.pi/180
+        self.threshold = 2.4
+        self.x_dot = 0.2
+        self.theta_dot = 03
+        background = []
+        self.unseen_background = []
+        self.unseen_background.extend([Atom(CARPOS, [str(x), "0"]) for x in range(1, threshold)])
+        # background.extend([Atom(SUCC, [str(i), str(i+1)]) for i in range(width - 1)])
+
+        super(cartPole, self).__init__(background, initial_state, actions)
+        
+
+        self.language = LanguageFrame(actions, extensional=[CARPOS, CARVEL, POLEVEL, POLEANG])
+        high = np.array[self.threshold*2, np.finfo(np.float32).max, self.theta_threshold*2, np.finfo(np.float32).max,]
+
+    @property
+    def all_states(self):
+        return[str(i) for i in range(self.high)]
+
+    def action_vec2symbol(self, action_vec):
+        return self.actions[np.argmax(action_vec)]
+
+    @property
+    def state(self):
+        return copy.deepcopy[self._state]
+
+    def state2atoms(self,state):
+        return[Atom(POLEANG,state)]
+
+    def next_step(self,action):
+        state = self._state
+        action = action.predicate
+        force = self.force_mag if action==1 else -self.force_mag
+        costheta = math.cos(theta)
+        sintheta = math.sin(theta)
+        temp = (force + self.polemass_length * theta_dot * theta_dot * sintheta) / self.total_mass
+        thetaacc = (self.gravity * sintheta - costheta* temp) / (self.length * (4.0/3.0 - self.masspole * costheta * costheta / self.total_mass))
+        xacc  = temp - self.polemass_length * thetaacc * costheta / self.total_mass
+        if self.kinematics_integrator == 'euler':
+            x  = x + self.tau * x_dot
+            x_dot = x_dot + self.tau * xacc
+            theta = theta + self.tau * theta_dot
+            theta_dot = theta_dot + self.tau * thetaacc
+        else: # semi-implicit euler
+            x_dot = x_dot + self.tau * xacc
+            x  = x + self.tau * x_dot
+            theta_dot = theta_dot + self.tau * thetaacc
+            theta = theta + self.tau * theta_dot
+        self.state = (x,x_dot,theta,theta_dot)
+        done =  x < -self.x_threshold \
+                or x > self.x_threshold \
+                or theta < -self.theta_threshold_radians \
+                or theta > self.theta_threshold_radians
+        done = bool(done)
+
+    def get_reward(self):
+        if not done:
+            reward = 1.0
+        elif self.steps_beyond_done is None:
+            self.steps_beyond_done = 0
+            reward = 1.0
+        else:
+            if self.steps_beyond_done == 0:
+                logger.warn("You are calling 'step()' even though this environment has already returned done = True. You should always call 'reset()' once you receive 'done = True' -- any further steps are undefined behavior.")
+            self.steps_beyond_done += 1
+            reward = 0.0
+
+
+
